94d6c8bcd3027f7c36fb98a37b6b1939ab124919
sinhyun Kim <ksh415622@naver.com>
2023-07-17 15:09:45 +0900

    feature: activerecord-import GEM 활용법 =

--- Gemfile
+++ Gemfile
@@ -55,6 +55,8 @@ gem 'ransack'
 
 gem 'activeadmin'
 
+gem 'activerecord-import'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin1@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+User.create!(email: '3@3.com',
+             password: 'password',
+             password_confirmation: 'password')
+
+
+User.create!(email: '4@4.com',
+             password: 'password',
+             password_confirmation: 'password')
+
+posts = []
+comments = []
+
+10.times do |x|
+  post = Post.new(title: "title#{x}", body: "content#{x}", user: User.first)
+  posts.push post
+  5.times do |y|
+    post.comments.new(body: "comment#{y}", user: User.last)
+    comments.push comment
+  end
+end
+
+# activerecord-import gem 활용법
+Post.import posts
+Comment.import comments
